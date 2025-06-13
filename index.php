f12b9efce59aaeeb23a9ee5c48b3555402eb547b
al1ness <fightviol@gmail.com>
2025-06-13 18:31:53 +0300

    do change

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 echo 'Start' . PHP_EOL;
 echo 'Continue' . PHP_EOL;
-echo 'End!' . PHP_EOL;
 
 for ($i = 1; $i <= 10; $i++) {
     echo "Mission №{$i} - ..." . PHP_EOL;
 }
+
+echo 'End' . PHP_EOL;
